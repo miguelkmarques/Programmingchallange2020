f07bf700c7fb752a629c1bdec90080490f0fc6bd
Miguel Marques <miguelkmarques@outlook.com>
2020-07-25 13:06:38 -0400

    Iniciando Projeto SeedMovieLens e aplicando Migrate no banco de dados

---
+++ SeedMovieLens/Program.cs
@@ -0,0 +1,41 @@
+﻿using Database.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+using System.Configuration;
+
+namespace SeedMovieLens
+{
+    class Program
+    {
+        private static readonly DbContextOptionsBuilder<ApplicationDbContext> OptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        public static ApplicationDbContext Context;
+        private static readonly IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Logger.Warn("start");
+            OptionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            var loggerFactory = LoggerFactory.Create(configure => { configure.AddNLog(); });
+            OptionsBuilder.UseLoggerFactory(loggerFactory);
+            Context = new ApplicationDbContext(OptionsBuilder.Options);
+
+            try
+            {
+                SeedData();
+            }
+            catch (Exception err)
+            {
+                Logger.Error(err);
+            }
+        }
+
+        static private void SeedData()
+        {
+            Context.Database.Migrate();
+        }
+    }
+}
